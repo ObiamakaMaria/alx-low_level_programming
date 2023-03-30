124f5f74b7a311fa95fa8f204860a1d72f3bacc2
ObiamakaMaria <obiamaka0101@gmail.com>
2023-03-29 18:00:30 -0700

    task 100-rot13.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -11,10 +11,9 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-
-
-
-
+char *rot13(char *);
+void print_number(int n);
+void print_buffer(char *b, int size);
 
 
 
