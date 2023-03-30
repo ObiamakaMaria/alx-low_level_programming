8073d9ab5109ff060064f0ece5a9719938c9ccd7
ObiamakaMaria <obiamaka0101@gmail.com>
2023-03-29 21:06:54 -0700

    task 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -33,11 +33,11 @@ void rev_string(char *n)
  * @r: pointer to buffer
  * @size_r: buffer size
  * Return: pointer to calling function
-*/
+ */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int overflow = 0, i = 0, j = 0, digits = 0;
+	{
+		int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
 	while (*(n1 + i) != '\0')
@@ -70,7 +70,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		j--;
 		i--;
 	}
-		if (digits == size_r)
+	if (digits == size_r)
 		return (0);
 	*(r + digits) = '\0';
 	rev_string(r);
