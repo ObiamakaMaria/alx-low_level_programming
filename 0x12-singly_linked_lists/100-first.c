1baa566589ba21c7c91a344a349b6d84c06d45f6
ObiamakaMaria <obiamaka0101@gmail.com>
2023-04-27 08:42:25 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - This function prints a collection of string  before the main
+ * function is executed
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n"); /*Continuation of the string */
+}
+
