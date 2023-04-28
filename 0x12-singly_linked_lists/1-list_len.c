e946e12d26a6ed7cde3ebd20533c638f35b45453
ObiamakaMaria <obiamaka0101@gmail.com>
2023-04-27 19:10:44 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,12 +11,12 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+	size_t count = 0; /*initializing count to 0 */
 
-	while (h)
+	while (h != NULL)
 	{
-	n++;
+	count++;
 	h = h->next;
 	}
-	return (n);
+	return (count);
 }
