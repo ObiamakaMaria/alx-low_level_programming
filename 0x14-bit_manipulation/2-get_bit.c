147335006875ae4d9dd18b1ce7e5490c2147a4ee
ObiamakaMaria <obiamaka0101@gmail.com>
2023-05-04 13:39:21 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,10 +5,10 @@
  * get_bit- This function returns the value of a bit at a given index.
  * @n: This represents the number to search for.
  * @index: This represents the index of the bits.
- * Description: In this function,index is the index, starting from 0 
+ * Description: In this function,index is the index, starting from 0
  * of the bit you want to get.
  *
- * Return: This function returns the value of the bit at index index or 
+ * Return: This function returns the value of the bit at index index or
  * -1 if an error occured
  */
 
@@ -22,5 +22,5 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	value_bit = (n >> index) & 1;
 
-	return(value_bit);/*This functions returns the number of bits*/
+	return (value_bit);/*This functions returns the number of bits*/
 }
