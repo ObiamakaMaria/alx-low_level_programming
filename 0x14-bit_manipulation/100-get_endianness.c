d2dd1a9569f24fd548c3e2bfc4964ab345a5fdab
ObiamakaMaria <obiamaka0101@gmail.com>
2023-05-07 17:07:30 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -14,6 +14,13 @@
 int get_endianness(void)
 {
 	unsigned int k = 1;
-	char *c = (char *) &k;
 
-	return (*k);
+	char *c = (char *)&k;
+
+	if (*c)
+	{
+		return (1); /* This is little Endian */
+	}
+	else
+		return (0); /* This is big Endian */
+}
