ae0db44d0d47bd0909057546fd64793b7999f9a0
ObiamakaMaria <obiamaka0101@gmail.com>
2023-09-08 14:50:50 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,17 +3,14 @@
 /**
  * hash_table_create - Creates a new hash table.
  * @size: The size of the array.
- *
  * Return: A pointer to the newly created hash table, or NULL on failure.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *newest_table = NULL;
 	unsigned long int i;
-
 	if (size < 1)
 		return (NULL);
-
 	newest_table = malloc(sizeof(hash_table_t));
 	if (newest_table == NULL)
 		return (NULL);
@@ -25,7 +22,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	newest_table->size = size;
-
 	for (i = 0; i < size; i++)
 	{
 		newest_table->array[i] = NULL;
