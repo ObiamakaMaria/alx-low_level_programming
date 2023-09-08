c9ed1a3c4bc0ccb192fcdea80448cdf185dbc7df
ObiamakaMaria <obiamaka0101@gmail.com>
2023-09-08 14:53:13 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *newest_table = NULL;
 	unsigned long int i;
-	
+
 	if (size < 1)
 		return (NULL);
 	newest_table = malloc(sizeof(hash_table_t));
@@ -17,14 +17,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	newest_table->array = malloc(sizeof(hash_node_t *) * size);
-	
+
 	if (newest_table->array == NULL)
 	{
 		free(newest_table);
 		return (NULL);
 	}
 	newest_table->size = size;
-	
+
 	for (i = 0; i < size; i++)
 	{
 		newest_table->array[i] = NULL;
