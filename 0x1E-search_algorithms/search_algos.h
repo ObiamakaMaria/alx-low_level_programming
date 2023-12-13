30943a6ffe0a58293119300847f9bd729b48b74e
ObiamakaMaria <obiamaka0101@gmail.com>
2023-12-13 02:05:55 -0800

    Header_file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,8 +13,9 @@ void print_array(int *array, size_t start, size_t end);
 int jump_search(int *array, size_t size, int value);
 size_t min(size_t a, size_t b);
 int interpolation_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t low, size_t high, int value);
+int new_binary_search(int *array, size_t low, size_t high, int value);
 int exponential_search(int *array, size_t size, int value);
+void print_array(int *array, size_t low, size_t high);
 
 
 
